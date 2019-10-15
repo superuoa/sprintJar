675ec4adb3a69fd488af2770a97268aa103a0c5d
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2019-10-15 09:58:34 +0700

    commit 1

--- src/main/java/com/example/demo/controller/RestTestController.java
+++ src/main/java/com/example/demo/controller/RestTestController.java
@@ -1,15 +1,30 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestTestController {
-
+	
 	@RequestMapping("/")
     public String restTest(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Return name= " + name ;
+       
+        String envStr = "";
+        
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+        	
+        	envStr += envName + "=" + env.get(envName) + "<br>";
+        	
+        }
+        
+		return envStr ;
         
     }
 	
